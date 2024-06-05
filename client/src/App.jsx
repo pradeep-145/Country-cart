81f400875a09a3bc85948519add750fd4387a066
pradeep sakthi <117750940+pradeep-145@users.noreply.github.com>
2024-06-05 19:17:52 +0530

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,14 +24,12 @@ function App() {
         <Route path='/login' element={<Login />}></Route>      
         <Route path='/register' element={<Register />}></Route>      
         <Route path='/forgot' element={<ForgotPass />}></Route>      
-      <Route path='/content' element={<Content />}></Route>
-      <Route path='/cart' element={<Cart />}></Route>
-      <Route path="/cart/:data" component={<Cart />} />
-      <Route path="/farmers" element={<Farmers />} />
-      <Route path="/flogin" element={<FarmerLogin />} />
-      <Route path="/fregister" element={<FarmerRegister />} />
-
-
+        <Route path='/content' element={<Content />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path="/cart/:data" component={<Cart />} />
+        <Route path="/farmers" element={<Farmers />} />
+        <Route path="/flogin" element={<FarmerLogin />} />
+        <Route path="/fregister" element={<FarmerRegister />} />
       </Routes>
       </BrowserRouter>
       
